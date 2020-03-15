23cb4e14c46026db27494d9568ccee2a448e488d
王海 <18912387311@163.com>
2020-03-15 11:01:17 +0800

    socket

--- SocketTemplate/Program.cs
+++ SocketTemplate/Program.cs
@@ -17,8 +17,12 @@ namespace SocketTemplate
         /// </summary>
         static void Main(string[] args)
         {
-            var service = new SocketAsyncEventService("127.0.0.1", 12345);
-            service.Start();
+            //var service = new SocketAsyncEventService("127.0.0.1", 12345);
+            //service.Start();
+
+            var service = new SocketDemo(100,50000);
+            service.Init();
+            service.Start(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 12345));
 
             while (true)
             {
